0ba11d25ed62a5a603b8928b972b2905dbcac418
paolavasileva22 <p.vasileva2018@pmg-pernik.com>
2022-02-08 13:28:03 +0200

    Zadachi

--- ZadMasivi1/ZadMasivi1/Program.cs
+++ ZadMasivi1/ZadMasivi1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ZadMasivi1
 {
@@ -6,7 +7,33 @@ namespace ZadMasivi1
     {
         static void Main(string[] args)
         {
-            
+            string[] arr = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            while(n-->0)
+            {
+                string[] command = Console.ReadLine().Split();
+                switch (command[0].ToLower())
+                {
+                    case "reverse":
+                        {
+                            Array.Reverse(arr);
+                            break;
+                        }
+                    case "distinct":
+                        {
+                            arr = arr.Distinct().ToArray();
+                            break;
+                        }
+                    case "replace":
+                        {
+                            int index = int.Parse(command[1]);
+                            string newWord = command[2];
+                            arr[index] = newWord;
+                            break;
+                        }
+                }
+                Console.WriteLine(string.Join(", ",arr));
+            }
         }
     }
 }
