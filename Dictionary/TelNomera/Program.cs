2d1f7a7fd5f732fed03424f98445f76f60894895
paolavasileva22 <p.vasileva2018@pmg-pernik.com>
2022-02-21 10:49:23 +0200

    Rechnici

---
+++ Dictionary/TelNomera/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TelNomera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> names = new Dictionary<string, string>();
+            while (true)
+            {
+                var input = Console.ReadLine().Split();
+
+                if (input[0] == "exit")
+                {
+                    foreach (var n in names)
+                    {
+                        Console.WriteLine($"{n.Key} -> {n.Value}");
+                    }
+                    break;
+                }
+                string name = input[0];
+                string phone = (input[1]);
+
+                if(!names.ContainsKey(name))
+                {
+                    names.Add(name, phone);
+                }
+                else
+                {
+                    names[name] = phone;
+                }
+            }
+
+        }
+    }
+}
+
+
