c6d0e0ce6765bf7d19e3830702b1f7c383e349ab
paolavasileva22 <p.vasileva2018@pmg-pernik.com>
2022-02-28 20:29:19 +0200

    Registraciq

---
+++ Registraciq/Registraciq/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Registraciq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> emails = new Dictionary<string, string>();
+            while (true)
+            {
+                var command = Console.ReadLine().Split();
+                if (command[0] == "Stop")
+                {
+                    break;
+                }
+                if (command[0] == "Add")
+                {
+                    var name = command[1];
+                    var email = command[2];
+                    if (!emails.ContainsKey(name))
+                    {
+                        emails.Add(name, email);
+                    }
+                    else
+                    {
+                        emails[name] = email;
+                    }
+                }
+                if (command[0] == "Sent")
+                {
+                    foreach (var item in emails)
+                    {
+                        var name = command[1];
+                        var email = item.Value;
+                        if (emails.ContainsKey(name))
+                        {
+                            Console.WriteLine($"{name} -> {email}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Contact {name} does not exists.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
